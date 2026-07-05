7e2865bdb5298b2d703d98ccedcf98e167debfc8
YahiaMXRT <imnotapotato.632@gmail.com>
2026-07-05 13:06:34 +0000

    setupbuild commit

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,8 @@ const m = ModAPI;
 const toggles = {
     fullbright: false
 };
-
+const mcSettings = ModAPI.settings
 m.settings.gammaSetting = 1.0
-m.addEventListener("update", () => {
-    m.drawString(m.getFPS(), 10,0,100.0)
-})
 m.addEventListener("sendchatmessage", (e: any) => {
     if (!e.message.startsWith("!")) return;
 
@@ -25,7 +22,31 @@ m.addEventListener("sendchatmessage", (e: any) => {
     } else if (e.message === "!help") {
         m.displayToChat(" §k help\n§3 !fb (FullBright)\n§2 !help (this text)")
     }
-    else if (e.message === "!help") {
-
+    else if (e.message.startsWith("!mode")) {
+        var args = {
+            a1: e.message.split(" ")[0],
+            a2: e.message.split(" ")[1]
+        }
+        if (args.a2 == "fps") {
+            mcSettings.fog = false
+            mcSettings.mipmapLevels = 0.0
+            mcSettings.clouds = 0.0
+            mcSettings.viewBobbing = false
+            mcSettings.fancyGraphics = false
+            mcSettings.chunkFix = true
+            mcSettings.enableVsync = false
+        }
+        else if (args.a2 == "fancy") {
+            mcSettings.renderDistanceChunks = 8
+            mcSettings.fog = true
+            mcSettings.mipmapLevels = 3.0
+            mcSettings.clouds = 100
+            mcSettings.viewBobbing = true
+            mcSettings.fancyGraphics = true
+            mcSettings.chunkFix = true
+            mcSettings.enableVsync = false
+        } else {
+            m.displayToChat("No mode exists with name: " + args.a2)
+        }
     }
 });
