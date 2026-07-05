f37e92df07f66101cb1036ab30c6ace5c520c26e
YahiaMXRT <YahiaMXRT@users.noreply.github.com>
2026-07-05 10:35:24 +0000

    Apply automatic changes

---
+++ out/EaglerForge-aqsndjas.js
@@ -0,0 +1,25 @@
+// src/main.ts
+var m = ModAPI;
+var toggles = {
+  fullbright: false
+};
+m.addEventListener("sendchatmessage", (e) => {
+  if (!e.message.startsWith("!"))
+    return;
+  e.preventDefault = true;
+  if (e.message === "!fb") {
+    if (!toggles.fullbright) {
+      toggles.fullbright = true;
+      m.settings.gammaSetting = 1000;
+      m.displayToChat("§a Fullbright enabled");
+    } else {
+      toggles.fullbright = false;
+      m.settings.gammaSetting = 0;
+      m.displayToChat("§c Fullbright disabled");
+    }
+  } else if (e.message === "!help") {
+    m.displayToChat(`   help   
+§3 !fb (FullBright)
+ §2 !help (this text)`);
+  }
+});
