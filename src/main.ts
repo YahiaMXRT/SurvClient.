65340c3e0db3ce96b7f7250e6dba1d90608deced
YahiaMXRT <imnotapotato.632@gmail.com>
2026-07-05 10:11:19 +0000

    s

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,11 @@ m.addEventListener("sendchatmessage", (e: any) => {
     if (e.message === "!fb") {
         if (!toggles.fullbright) {
             toggles.fullbright = true;
+            m.settings.gammaSetting = 1000.0
             m.displayToChat("§9Fullbright enabled");
         } else {
             toggles.fullbright = false;
+            m.settings.gammaSetting = 0.0
             m.displayToChat("§3Fullbright disabled");
         }
     }
