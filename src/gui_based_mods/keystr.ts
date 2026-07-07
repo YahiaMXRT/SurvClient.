cd93ccc8142f7e6129cfe02675bdc7e53a7865b1
YahiaMXRT <imnotapotato.632@gmail.com>
2026-07-07 08:09:06 +0000

    setupbuild commit

--- src/gui_based_mods/keystr.ts
+++ src/gui_based_mods/keystr.ts
@@ -6,19 +6,6 @@ document.head.appendChild(a)
 export var initKeystrokes = () => {
     if (toggles.keystrokes) {
         const keystrokes = document.createElement("div");
-        keystrokes.id = "keystrokes_"
-        keystrokes.style.position = "absolute";
-        keystrokes.style.top = "10px";
-        keystrokes.style.display ="flex";
-        keystrokes.style.flexDirection = "column"
-        keystrokes.style.gap = "2px"
-        keystrokes.style.justifyContent = "center"
-        keystrokes.style.right = "10px";
-        keystrokes.style.fontSize = "20px";
-        keystrokes.style.color = "white";
-        keystrokes.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-        keystrokes.style.padding = "5px";
-        keystrokes.style.borderRadius = "5px";
         document.body.appendChild(keystrokes);
         var keysPressed: { [key: string]: boolean } = {w: false, a: false, s: false, d: false};
         window.addEventListener("keydown", (e) => {
