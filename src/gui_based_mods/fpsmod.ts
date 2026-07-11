d67d9a14db9bd93ea9c592b48272160480389ab6
YahiaMXRT <imnotapotato.632@gmail.com>
2026-07-11 22:42:07 +0300

    modified:   out/Client.js 	modified:   src/gui_based_mods/fpsmod.ts 	modified:   src/gui_based_mods/fpsmodCSS.css     on fps mod disable, ModAPI event listener wouldnt be duplicated     css improvements

--- src/gui_based_mods/fpsmod.ts
+++ src/gui_based_mods/fpsmod.ts
@@ -5,13 +5,14 @@ export var initFpsMod = () => {
     if (toggles.fpsMod == true) {
         var fps = document.createElement('div');
         fps.id = "fpsm_"
-        ModAPI.addEventListener("update", () => {
+        var tempEvent = ModAPI.addEventListener("update", () => {
             fps.innerHTML = `<span class="fpsm-inner">${ModAPI.getFPS()}</span>`
         })
         document.body.appendChild(fps)
     } else if (toggles.fpsMod == false) {
         if (document.getElementById("fpsm_")) {
             document.getElementById("fpsm_").remove()
+            tempEvent = undefined
         } else {
             return 0
         }
